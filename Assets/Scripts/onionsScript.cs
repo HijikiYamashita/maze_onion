335e07fbcc4823cb5ee26dd52f6f9890e4018cdf
HijikiYamashita <godenter792@gmail.com>
2020-12-15 21:09:38 +0900

    ハハッ！

--- Assets/Scripts/onionsScript.cs
+++ Assets/Scripts/onionsScript.cs
@@ -1,29 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class onionsScript : MonoBehaviour
 {
     public int speed;
 
-    bool stalking = false;
-    public bool a = false;
+    GameObject player;
+
+    [SerializeField] bool stalking = false;
 
     void Start()
     {
-        
+        player = GameObject.Find("Player");
+        transform.Find("AroundCollider").gameObject.SetActive(false);
     }
 
     void Update()
     {
-
+        if (stalking == true)
+        {
+            this.gameObject.GetComponent<NavMeshAgent>().destination = player.transform.position;
+        }
     }
 
     void OnTriggerStay(Collider col)
     {
         if (col.gameObject.tag == "Player")
         {
+            transform.Find("AroundCollider").gameObject.SetActive(true);
             stalking = true;
         }
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            transform.Find("AroundCollider").gameObject.SetActive(false);
+            Invoke("stalkingOff", 1);
+        }
+    }
+
+    void stalkingOff()
+    {
+        stalking = false;
     }
 }
